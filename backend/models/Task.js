2433fde5c279da46c7b7ee6f9d5bdaf07199e303
Tanisha borana <101504198+Tanisha-b3@users.noreply.github.com>
2026-04-24 13:25:02 +0530

    Fontend backend readme

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,6 +1,6 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
-import User from './User.js';
+import User from './user.js';
 
 const Task = sequelize.define('Task', {
   id: {

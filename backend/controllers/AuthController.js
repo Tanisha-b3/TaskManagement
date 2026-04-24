e31e33ade1651e6f26de1c42511346f55e054d4a
Tanisha borana <101504198+Tanisha-b3@users.noreply.github.com>
2026-04-24 13:20:33 +0530

    Fontend backend readme

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import { validationResult } from 'express-validator';
 
 const generateToken = (id) => {
